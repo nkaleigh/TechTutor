d90862acaa162f7cd92e208a6ba41a3d36518532
Kaleigh Niemela <nkaleigh@users.noreply.github.com>
2017-05-03 19:12:30 -0600

    5/3:7

--- development/js/services/tutorlistService.js
+++ development/js/services/tutorlistService.js
@@ -1,60 +1,90 @@
 angular.module("App").service("tutorlistService", function ($http, $q, $state) {
     var tutors = [{
-        name: "Kaleigh Niemela",
+        firstname: "Kaleigh",
+        lastname: "Niemela",
+        tutorId: 1,
         location: "Orem, Utah",
         img: "x",
         title: "Web Developer Tutor",
         bio: "I recently completed Dev Mountain's Web Development course. I have grown to love coding and also teaching others more about code. I am available most evening hours and I am flexible with transportation.",
-        rate: "free"
+        rate: "free",
+        rating: 5
     }, {
-        name: "Christopher Vosters",
+        firstname: "Christopher",
+        lastname: "Vosters",
+        tutorId: 2,
         location: "Orem, Utah",
         img: "x",
         title: "Javascript Tutor",
         bio: "I recently completed Dev Mountain's Web Development course. I have grown to love coding and also teaching others more about code. I am available most evening hours and I am flexible with transportation.",
-        rate: "$10/hr"
+        rate: "$10/hr",
+        rating: 4,
     }, {
-        name: "Oshion Niemela",
+        firstname: "Oshion",
+        lastname: "Niemela",
+        tutorId: 3,
         location: "Provo, Utah",
         img: "x",
         title: "Python Tutor",
         bio: "I recently completed Dev Mountain's Web Development course. I have grown to love coding and also teaching others more about code. I am available most evening hours and I am flexible with transportation.",
-        rate: "$20/hr"
+        rate: "$20/hr",
+        rating: 5,
+
     }, {
-        name: "Ed Smith",
+        firstname: "Ed",
+        lastname: "Smith",
+        tutorId: 4,
         location: "Orem, Utah",
         img: "x",
         title: "Angular Tutor",
         bio: "I recently completed Dev Mountain's Web Development course. I have grown to love coding and also teaching others more about code. I am available most evening hours and I am flexible with transportation.",
-        rate: "$12/hr"
+        rate: "$12/hr",
+        rating: 2,
+
     }, {
-        name: "Anna Wright",
+        firstname: "Anna",
+        lastname: "Wright",
+        tutorId: 5,
         location: "Orem, Utah",
         img: "x",
         title: "Web Developer Tutor",
         bio: "I recently completed Dev Mountain's Web Development course. I have grown to love coding and also teaching others more about code. I am available most evening hours and I am flexible with transportation.",
-        rate: "$15/hr"
+        rate: "$15/hr",
+        rating: 1,
+
     }, {
-        name: "Luis Jones",
+        firstname: "Luis",
+        lastname: "Jones",
+        tutorId: 6,
         location: "Orem, Utah",
         img: "x",
         title: "React Tutor",
         bio: "I recently completed Dev Mountain's Web Development course. I have grown to love coding and also teaching others more about code. I am available most evening hours and I am flexible with transportation.",
-        rate: "$10/hr"
+        rate: "$10/hr",
+        rating: 4,
+
     }, {
-        name: "Miriam Nelson",
+        firstname: "Miriam",
+        lastname: "Nelson",
+        tutorId: 7,
         location: "Orem, Utah",
         img: "x",
         title: "Node.js Tutor",
         bio: "I recently completed Dev Mountain's Web Development course. I have grown to love coding and also teaching others more about code. I am available most evening hours and I am flexible with transportation.",
-        rate: "$100/hr"
+        rate: "$100/hr",
+        rating: 4,
+
     }, {
-        name: "Laura Andrews",
+        firstname: "Laura",
+        lastname: "Andrews",
+        tutorId: 8,
         location: "Orem, Utah",
         img: "x",
         title: "Web Developer Tutor",
         bio: "I recently completed Dev Mountain's Web Development course. I have grown to love coding and also teaching others more about code. I am available most evening hours and I am flexible with transportation.",
-        rate: "$70/hr"
+        rate: "$70/hr",
+        rating: 2,
+
     }]
 
     this.getData = function () {
